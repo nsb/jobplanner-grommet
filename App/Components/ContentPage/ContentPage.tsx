c14fdb0ea0d40abadda896183dbbb2683ec3fb89
Niels Sandholt Busch <niels.busch@gmail.com>
2016-12-17 14:58:11 +0100

    remove unused code

--- App/Components/ContentPage/ContentPage.tsx
+++ App/Components/ContentPage/ContentPage.tsx
@@ -1,48 +1,15 @@
 import * as React from "react";
-import ContentHeader from "./ContentHeader/ContentHeader";
-import ContentBody from "./ContentBody/ContentBody";
-import CommonStore from "../../Stores/CommonStore";
-import  * as CommonActionCreators from "../../ActionCreators/CommonActionCreators";
-import SmartComponent from "./../SmartComponent";
 
 /* tslint:disable:no-any */
 const styles: any = require("./ContentPage.module.less");
 /* tslint:enable:no-any */
 
-interface IContentPageState {
-   bodyTitle: string;
-   bodySummary: string;
-   sayHelloCount: number;
-}
+export default class ContentPage extends React.Component<{}, {}> {
 
-export default class ContentPage extends SmartComponent<{}, IContentPageState> {
-    constructor() {
-        super(CommonStore);
-    }
-
-    doRender(): React.ReactElement<{}> {
-        const headerTitle: string = "Welcome to Lorem Ipsum";
+    render(): React.ReactElement<{}> {
 
         return <div className={styles.container}>
-                   <ContentHeader isActive={true} title={headerTitle} />
-                   <ContentBody ref="contentBodyRef" title={this.state.bodyTitle} summary={this.state.bodySummary}>
-                       <div className={styles.hello}>
-                           <button className={styles.button} onClick={() => this.onButtonClick()}>Say Hello!</button>
-                           <div className={styles.message}>You said hello {this.state.sayHelloCount} time(s)</div>
-                       </div>
-                   </ContentBody>
+                Hejsa
                </div>;
-    }
-
-    protected getState(): IContentPageState {
-        return {
-            bodyTitle: CommonStore.getBodyTitle(),
-            bodySummary: CommonStore.getBodySummary(),
-            sayHelloCount: CommonStore.getSayHelloCount()
-        };
-    }
-
-    private onButtonClick(): void {
-        CommonActionCreators.sayHello();
     }
 }
--- App/Stores/CommonStore.ts
+++ App/Stores/CommonStore.ts
@@ -1,43 +1,10 @@
+import {useStrict} from "mobx";
 import BaseStore from "./BaseStore";
-import AppLoadedAction from "../Actions/AppLoadedAction";
-import SayHelloAction from "../Actions/SayHelloAction";
+useStrict(true);
 
 class CommonStore extends BaseStore {
-    private bodyTitle: string;
-    private bodySummary: string;
-    private sayHelloCount: number = 0;
+    // @observable private token: string;
 
-    constructor() {
-        super();
-
-        this.addActionCallback(AppLoadedAction, this.onAppLoaded);
-        this.addActionCallback(SayHelloAction, this.onSayHello);
-    }
-
-    getBodyTitle(): string {
-        if (this.bodyTitle) {
-            return this.bodyTitle.toUpperCase();
-        }
-
-        return "";
-    }
-
-    getBodySummary(): string {
-        return this.bodySummary;
-    }
-
-    getSayHelloCount(): number {
-        return this.sayHelloCount;
-    }
-
-    private onAppLoaded(action: AppLoadedAction): void {
-        this.bodyTitle = action.bodyTitle;
-        this.bodySummary = action.bodySummary;
-    }
-
-    private onSayHello(): void {
-        this.sayHelloCount++;
-    }
 }
 
 export default new CommonStore();
