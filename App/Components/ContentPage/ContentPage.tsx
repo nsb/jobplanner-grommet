32f31700639c7ec0980f389d37f7580c4746c81e
Niels Sandholt Busch <niels.busch@gmail.com>
2016-12-17 23:51:52 +0100

    more grommet compoments

--- App/Components/ContentPage/ContentPage.tsx
+++ App/Components/ContentPage/ContentPage.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
 /* tslint:disable:no-any */
 import App = require("grommet/components/App");
-import Button = require("grommet/components/Button");
+import Box = require("grommet/components/Box");
+import Header = require("grommet/components/Header");
+import Footer = require("grommet/components/Footer");
+import Meter = require("grommet/components/Meter");
+import Title = require("grommet/components/Title");
 /* tslint:enable:no-any */
 
 export default class ContentPage extends React.Component<{}, {}> {
 
     render(): React.ReactElement<{}> {
 
-        return <App>
-                Hejsa
-                <Button />
-               </App>;
+        return <App centered={false}>
+                <Header direction="row" justify="between"
+                  pad={{horizontal: "medium"}}>
+                  <Title>Grommet standalone</Title>
+                </Header>
+                <Box pad="medium">
+                  <Meter value={40} />
+                </Box>
+                <Footer primary={true} appCentered={true} direction="column"
+                  align="center" pad="small" colorIndex="grey-1">
+                  <p>
+                    Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!
+                  </p>
+                </Footer>
+              </App>;
     }
 }
